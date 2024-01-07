a01a8c56c47716c633e5188658a1205713528942
Youngjoon Park <pyj2025@gmail.com>
2024-01-07 15:31:14 -0800

    iteration questions

---
+++ Prob6075.py
@@ -0,0 +1,16 @@
+# 4
+
+# 0
+# 1
+# 2
+# 3
+# 4
+
+a = int(input())
+idx = 0
+
+while(True):
+    if(idx == a + 1):
+        break
+    print(idx)
+    idx +=1
---
+++ Prob6076.py
@@ -0,0 +1,12 @@
+# 4
+
+# 0
+# 1
+# 2
+# 3
+# 4
+
+n = int(input())
+
+for i in range(n+1):
+    print(i)
